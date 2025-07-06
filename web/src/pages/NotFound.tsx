c2dda42be94c47454f1d464453a7ed4a0321a886
Vitor Nere <vitor.nere@hotmail.com>
2025-07-06 20:38:49 -0300

    Enhance NotFound and SlugRedirect pages with improved UI
    - Updated NotFound page to include a 404 icon and a user-friendly message.
    - Enhanced SlugRedirect page with a logo and clearer redirection instructions.
    - Both pages now utilize Tailwind CSS for better styling and layout.

--- web/src/pages/NotFound.tsx
+++ web/src/pages/NotFound.tsx
@@ -1,3 +1,13 @@
+import NotFoundIcon from "@/assets/404.svg";
+
 export default function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <div className="h-dvh flex justify-center items-center">
+      <div className="flex flex-col gap-4 items-center bg-gray-100 rounded-lg mx-4 p-8 lg:p-16 lg:mx-0 text-center max-w-lg">
+        <img src={NotFoundIcon} alt="Brev" className="w-[12.125rem] h-[5.313rem]" />
+        <p className="typography-xl text-gray-600">Link não encontrado</p>
+        <span className="typography-md text-gray-500">O link que você está tentando acessar não existe, foi removido ou é uma URL inválida. Saiba mais em <a className="typography-md text-blue">abrev.ly</a></span>
+      </div>
+    </div>
+  )
 }
--- web/src/pages/SlugRedirect.tsx
+++ web/src/pages/SlugRedirect.tsx
@@ -1,6 +1,17 @@
 import { useParams } from "wouter";
+import LogoIcon from "@/assets/Logo_Icon.svg";
 
 export default function SlugRedirect() {
   const { slug } = useParams();
-  return <div>SlugRedirect {slug}</div>
+
+  return (
+    <div className="h-dvh flex justify-center items-center">
+      <div className="flex flex-col gap-4 items-center bg-gray-100 rounded-lg mx-4 p-8 lg:p-16 lg:mx-0 text-center">
+        <img src={LogoIcon} alt="Brev" className="w-12 h-12" />
+        <p className="typography-xl text-gray-600">Redirecionando...</p>
+        <span className="typography-md text-gray-500">O link será aberto automaticamente em alguns instantes.</span>
+        <span className="typography-md text-gray-500">Não foi redirecionado? <a className="typography-md text-blue">Acesse aqui</a></span>
+      </div>
+    </div>
+  )
 }
